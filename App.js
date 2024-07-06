15d75858b2057a8edc79147b0bcb4d7f1fc969ff
Zhi Ling <156146496+zlnortheastern@users.noreply.github.com>
2024-07-06 13:55:09 +0800

    Implement working app with server

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import express from 'express'
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 const app = express();
-app.use(express.json());
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
