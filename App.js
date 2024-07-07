066c02b735b728ad55d9654ba2f59b85280601c7
Zhi Ling <156146496+zlnortheastern@users.noreply.github.com>
2024-07-07 13:22:37 +0800

    Implement Kanbas server

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import express from 'express'
+import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
