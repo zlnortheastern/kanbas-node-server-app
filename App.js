432d195fb6aa74544ff25e9574618ac3a701de69
Zhi Ling <156146496+zlnortheastern@users.noreply.github.com>
2024-07-18 16:29:54 +0800

    Implement user server

--- App.js
+++ App.js
@@ -5,9 +5,17 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./User/routes.js";
+
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
---
+++ User/routes.js
@@ -0,0 +1,39 @@
+import * as dao from "./dao.js";
+let currentUser = null;
+export default function UserRoutes(app) {
+  const createUser = async (req, res) => {};
+  const deleteUser = async (req, res) => {};
+  const findAllUsers = async (req, res) => {
+    const { role, name } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
+    const users = await dao.findAllUsers();
+    res.json(users);
+  };
+  const findUserById = async (req, res) => {
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+  };
+  const updateUser = async (req, res) => {};
+  const signup = async (req, res) => {};
+  const signin = async (req, res) => {};
+  const signout = (req, res) => {};
+  const profile = async (req, res) => {};
+  app.post("/api/users", createUser);
+  app.get("/api/users", findAllUsers);
+  app.get("/api/users/:userId", findUserById);
+  app.put("/api/users/:userId", updateUser);
+  app.delete("/api/users/:userId", deleteUser);
+  app.post("/api/users/signup", signup);
+  app.post("/api/users/signin", signin);
+  app.post("/api/users/signout", signout);
+  app.post("/api/users/profile", profile);
+}
