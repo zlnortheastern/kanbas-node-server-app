022a88eee44ecd0af6c6c2095f7e0d268a7ef316
Zhi Ling <156146496+zlnortheastern@users.noreply.github.com>
2024-07-26 17:55:47 +0800

    update module and course database server

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,28 +1,22 @@
-import Database from "../Database/index.js";
+import * as dao from "./dao.js";
 export default function CourseRoutes(app) {
-  app.put("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    const course = req.body;
-    Database.courses = Database.courses.map((c) =>
-      c._id === id ? { ...c, ...course } : c
-    );
-    res.sendStatus(204);
-  });
-  
-  app.delete("/api/courses/:id", (req, res) => {
-    const { id } = req.params;
-    Database.courses = Database.courses.filter((c) => c._id !== id);
-    res.sendStatus(204);
+  app.put("/api/courses/:id", async (req, res) => {
+    const course = await dao.updateCourse(req.params.id, req.body);
+    res.json(course);
   });
 
-  app.post("/api/courses", (req, res) => {
-    const course = { ...req.body, _id: new Date().getTime().toString() };
-    Database.courses.push(course);
-    res.send(course);
+  app.delete("/api/courses/:id", async (req, res) => {
+    const status = await dao.deleteCourse(req.params.id);
+    res.json(status);
   });
 
-  app.get("/api/courses", (req, res) => {
-    const courses = Database.courses;
+  app.post("/api/courses", async (req, res) => {
+    const course = await dao.createCourse(req.body);
+    res.json(course);
+  });
+
+  app.get("/api/courses", async (req, res) => {
+    const courses = await dao.findAllCourses();
     res.send(courses);
   });
 }
